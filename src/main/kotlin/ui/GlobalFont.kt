14d68544ef33251153085d36ca73122032fdca24
fredoverflow <fredoverflow@users.noreply.github.com>
2019-09-22 13:25:34 +0200

    global font size depends on screen height

--- src/main/kotlin/ui/GlobalFont.kt
+++ src/main/kotlin/ui/GlobalFont.kt
@@ -1,5 +1,14 @@
 package ui
 
 import java.awt.Font
+import java.awt.Toolkit
 
-val globalFont = Font(Font.MONOSPACED, Font.PLAIN, if (freditor.Front.size == "large") 24 else 16)
+val globalFont: Font = {
+    val screenHeight = Toolkit.getDefaultToolkit().screenSize.height
+    val fontSize = when {
+        screenHeight < 1000 -> 16
+        screenHeight < 1500 -> 24
+        else -> 36
+    }
+    Font(Font.MONOSPACED, Font.PLAIN, fontSize)
+}()
