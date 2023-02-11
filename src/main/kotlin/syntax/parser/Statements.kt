9f768cdd46a15e84005d7524acdbf5d66870538e
fredoverflow <fredoverflow@users.noreply.github.com>
2023-02-11 20:11:37 +0100

    explain for-loop syntax

--- src/main/kotlin/syntax/parser/Statements.kt
+++ src/main/kotlin/syntax/parser/Statements.kt
@@ -1,5 +1,7 @@
 package syntax.parser
 
+import common.Diagnostic
+import syntax.lexer.Token
 import syntax.lexer.TokenKind.*
 import syntax.tree.*
 
@@ -18,9 +20,10 @@ fun Parser.statement(): Statement = when (current) {
     DO -> Do(accept(), statement() before WHILE, condition()).semicolon()
 
     FOR -> symbolTable.scoped {
+        val f0r = accept() before OPENING_PAREN
         For(
-            accept() before OPENING_PAREN,
-            forInit(),
+            f0r,
+            forInit(f0r),
             ::expression optionalBefore SEMICOLON,
             ::expression optionalBefore CLOSING_PAREN,
             statement()
@@ -60,18 +63,40 @@ fun Parser.statement(): Statement = when (current) {
     else -> ExpressionStatement(expression()).semicolon()
 }
 
-private fun Parser.forInit(): Statement? = when (current) {
+private fun Parser.forInit(f0r: Token): Statement? = when (current) {
     SEMICOLON -> null.semicolon()
 
     TYPEDEF, EXTERN, STATIC, AUTO, REGISTER, CONST, VOLATILE,
     VOID, CHAR, SHORT, INT, LONG, FLOAT, DOUBLE, SIGNED, UNSIGNED,
-    STRUCT, UNION, ENUM -> declaration()
+    STRUCT, UNION, ENUM -> forInitDeclaration(f0r)
 
     IDENTIFIER -> when {
-        isTypedefName(token) -> declaration()
+        isTypedefName(token) -> forInitDeclaration(f0r)
 
-        else -> ExpressionStatement(expression()).semicolon()
+        else -> forInitExpressionStatement(f0r)
     }
 
-    else -> ExpressionStatement(expression()).semicolon()
+    else -> forInitExpressionStatement(f0r)
+}
+
+private const val FOR_LOOP_SYNTAX = "for (init; condition; update)\n         ^          ^\nSemicolons, NOT commas!"
+
+private fun Parser.forInitDeclaration(f0r: Token): Statement {
+    try {
+        return declaration()
+    } catch (diagnostic: Diagnostic) {
+        if (diagnostic.message.endsWith(" was already declared elsewhere")) {
+            f0r.error(FOR_LOOP_SYNTAX)
+        } else {
+            throw diagnostic
+        }
+    }
+}
+
+private fun Parser.forInitExpressionStatement(f0r: Token): Statement {
+    val result = ExpressionStatement(expression())
+    if (current == CLOSING_PAREN) {
+        f0r.error(FOR_LOOP_SYNTAX)
+    }
+    return result.semicolon()
 }
